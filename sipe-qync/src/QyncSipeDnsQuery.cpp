6739635bdf2104ef54483277717d9f930f2b900e
Chen Mulong <chenmulong@gmail.com>
2014-08-25 22:06:18 +0800

    QyncSipeDnsQuery clean up.
     1. Fix possible leak with cancel.
     2. Other minor problems.

--- sipe-qync/src/QyncSipeDnsQuery.cpp
+++ sipe-qync/src/QyncSipeDnsQuery.cpp
@@ -9,49 +9,45 @@ class QyncSipeDnsQuery : QObject
 {
     Q_OBJECT
 
-    public:
-        QyncSipeDnsQuery(sipe_core_public *sipe_public,
-                sipe_dns_resolved_cb callback, gpointer data,
-                QObject *parent = 0);
+public:
+    QyncSipeDnsQuery(const sipe_core_public *sipe_public,
+            const sipe_dns_resolved_cb callback, const gpointer data, QObject *parent = 0);
 
-        void queryA(const gchar *hostname, guint port);
-        void querySrv(const gchar *protocol, const char *transport, const char *domain);
+    void queryA(const gchar *hostname, guint port);
+    void querySrv(const gchar *protocol, const char *transport, const char *domain);
+    void cancel();
 
-    private:
-        ~QyncSipeDnsQuery();
-        void returnResult(QString hostname, quint16 port);
-        void returnEmptyResult();
+private:
+    ~QyncSipeDnsQuery();
+    void returnResult(const QString &hostname, quint16 port);
+    void returnEmptyResult();
 
-        struct sipe_core_public *mSipePublic;
-        sipe_dns_resolved_cb mCallback;
-        gpointer mData;
-        QDnsLookup *mDns;
-        guint mPort;
+    //Not used for now.
+    //struct sipe_core_public *mSipePublic;
+    sipe_dns_resolved_cb mCallback;
+    gpointer mData;
+    QDnsLookup mDns;
+    guint mPort;
 
-    private slots:
-        void handleQueryA();
-        void handleQuerySrv();
+private slots:
+    void handleQueryA();
+    void handleQuerySrv();
 };
 
-QyncSipeDnsQuery::QyncSipeDnsQuery(sipe_core_public *sipe_public,
-        sipe_dns_resolved_cb callback,  gpointer data,
-        QObject *parent)
-        :QObject(parent), mPort(0)
+QyncSipeDnsQuery::QyncSipeDnsQuery(const sipe_core_public * /*sipe_public*/,
+        const sipe_dns_resolved_cb callback,  const gpointer data, QObject *parent)
+        :QObject(parent),
+        mCallback(callback), mData(data), mDns(this), mPort(0)
 {
-    mSipePublic = sipe_public;
-    mCallback = callback;
-    mData = data;
-
-    mDns = new QDnsLookup(this);
+    //mSipePublic = sipe_public;
 }
 
 QyncSipeDnsQuery::~QyncSipeDnsQuery()
 {
-    mDns->abort();
-    delete mDns;
+    mDns.abort();
 }
 
-void QyncSipeDnsQuery::returnResult(QString hostname, quint16 port)
+void QyncSipeDnsQuery::returnResult(const QString &hostname, quint16 port)
 {
     mCallback(mData, hostname.toStdString().c_str(), port);
     delete this;
@@ -66,38 +62,43 @@ void QyncSipeDnsQuery::returnEmptyResult()
 void QyncSipeDnsQuery::querySrv(const gchar *protocol, const char *transport,
         const char *domain)
 {
-    connect(mDns, SIGNAL(finished()),
+    connect(&mDns, SIGNAL(finished()),
             this, SLOT(handleQuerySrv()));
 
-    mDns->setType(QDnsLookup::SRV);
+    mDns.setType(QDnsLookup::SRV);
     QString name = QString("_%1._%2.%3").arg(protocol, transport, domain);
-    mDns->setName(name);
-    mDns->lookup();
+    mDns.setName(name);
+    mDns.lookup();
+}
+
+void QyncSipeDnsQuery::cancel()
+{
+    delete this;
 }
 
 void QyncSipeDnsQuery::queryA(const gchar *hostname, guint port)
 {
-    connect(mDns, SIGNAL(finished()),
+    connect(&mDns, SIGNAL(finished()),
             this, SLOT(handleQueryA()));
 
     mPort = port;
-    mDns->setType(QDnsLookup::A);
-    mDns->setName(hostname);
-    mDns->lookup();
+    mDns.setType(QDnsLookup::A);
+    mDns.setName(hostname);
+    mDns.lookup();
 }
 
 void QyncSipeDnsQuery::handleQuerySrv()
 {
     // Check the lookup succeeded.
-    if (mDns->error() != QDnsLookup::NoError) {
-        qWarning("DNS lookup failed %d", mDns->error());
+    if (mDns.error() != QDnsLookup::NoError) {
+        qWarning("DNS lookup failed %d", mDns.error());
 
         returnEmptyResult();
         return;
     }
 
     // Handle the results.
-    foreach (const QDnsServiceRecord &record, mDns->serviceRecords()) {
+    foreach (auto &record, mDns.serviceRecords()) {
         returnResult(record.target(), record.port());
         break;
     }
@@ -106,50 +107,53 @@ void QyncSipeDnsQuery::handleQuerySrv()
 void QyncSipeDnsQuery::handleQueryA()
 {
     // Check the lookup succeeded.
-    if (mDns->error() != QDnsLookup::NoError) {
-        qWarning("DNS lookup failed %d", mDns->error());
+    if (mDns.error() != QDnsLookup::NoError) {
+        qWarning("DNS lookup failed %d", mDns.error());
 
         returnEmptyResult();
         return;
     }
 
     // Handle the results.
-    foreach (const QDnsHostAddressRecord &record, mDns->hostAddressRecords()) {
+    foreach (auto &record, mDns.hostAddressRecords()) {
         returnResult(record.value().toString(), mPort);
         break;
     }
 }
 struct sipe_dns_query *sipe_backend_dns_query_srv(struct sipe_core_public *sipe_public,
-						  const gchar *protocol,
-						  const gchar *transport,
-						  const gchar *domain,
-						  sipe_dns_resolved_cb callback,
-						  gpointer data)
+        const gchar *protocol,
+        const gchar *transport,
+        const gchar *domain,
+        sipe_dns_resolved_cb callback,
+        gpointer data)
 {
-	SIPE_DEBUG_INFO("sipe_backend_dns_query_srv: %s %s %s", protocol, transport, domain);
+    SIPE_DEBUG_INFO("sipe_backend_dns_query_srv: %s %s %s", protocol, transport, domain);
     QyncSipeDnsQuery *query = new QyncSipeDnsQuery(sipe_public, callback, data,
             (QObject *)(sipe_public->backend_private));
     query->querySrv(protocol, transport, domain);
 
-    return (struct sipe_dns_query *)query;
+    return reinterpret_cast<struct sipe_dns_query *>(query);
 }
 
 struct sipe_dns_query *sipe_backend_dns_query_a(struct sipe_core_public *sipe_public,
-						const gchar *hostname,
-						guint port,
-						sipe_dns_resolved_cb callback,
-						gpointer data)
+        const gchar *hostname,
+        guint port,
+        sipe_dns_resolved_cb callback,
+        gpointer data)
 {
-	SIPE_DEBUG_INFO("sipe_backend_dns_query_a: %s %d", hostname, port);
+    SIPE_DEBUG_INFO("sipe_backend_dns_query_a: %s %d", hostname, port);
     QyncSipeDnsQuery *query = new QyncSipeDnsQuery(sipe_public, callback, data);
     query->queryA(hostname, port);
 
-    return (struct sipe_dns_query *)query;
+    return reinterpret_cast<struct sipe_dns_query *>(query);
 }
 
 void sipe_backend_dns_query_cancel(struct sipe_dns_query *query)
 {
-	SIPE_DEBUG_INFO_NOFORMAT("sipe_backend_dns_query_cancel");
+    SIPE_DEBUG_INFO_NOFORMAT("sipe_backend_dns_query_cancel");
+
+    QyncSipeDnsQuery *dnsQuery = reinterpret_cast<QyncSipeDnsQuery *>(query);
+    dnsQuery->cancel();
 }
 
 #include "QyncSipeDnsQuery.moc"
