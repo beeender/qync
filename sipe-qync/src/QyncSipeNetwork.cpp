fc5f061eddfd1393ee1dcea2df089f54e6b79b9e
Chen Mulong <chenmulong@gmail.com>
2014-08-25 23:25:22 +0800

    Cache the local ip address.
    The sipe-backend will call it always.
    And warning fix.

--- sipe-qync/src/QyncSipeNetwork.cpp
+++ sipe-qync/src/QyncSipeNetwork.cpp
@@ -1,46 +1,51 @@
 #include <glib.h>
 #include <QHostAddress>
 #include <QNetworkInterface>
+#include <algorithm>
 
 #include "sipe-backend.h"
 
-const gchar *sipe_backend_network_ip_address(struct sipe_core_public *sipe_public)
+const gchar *sipe_backend_network_ip_address(struct sipe_core_public * /*sipe_public*/)
 {
-    QString addr;
-    foreach (const QHostAddress &address, QNetworkInterface::allAddresses()) {
-        if (address.protocol() == QAbstractSocket::IPv4Protocol &&
-                address != QHostAddress(QHostAddress::LocalHost)) {
-            addr = address.toString();
-            qDebug() << address.toString();
-        }
-    }
-    SIPE_DEBUG_INFO("sipe_backend_network_ip_address %s", addr.toStdString().c_str());
-    return addr.toStdString().c_str();
+    static gchar addr[16] = {0};
+    if (addr[0]) return addr;
+
+    auto l = QNetworkInterface::allAddresses();
+    auto it = std::find_if(l.begin(), l.end(),
+            [](const QHostAddress &a) {
+                return (a.protocol() == QAbstractSocket::IPv4Protocol &&
+                    a!= QHostAddress(QHostAddress::LocalHost));
+            });
+    if (it == l.end()) return "";
+
+    g_strlcpy(addr, (*it).toString().toStdString().c_str(), 16);
+    SIPE_DEBUG_INFO("sipe_backend_network_ip_address %s", addr);
+    return addr;
 }
 
 struct sipe_backend_listendata *
-sipe_backend_network_listen_range(unsigned short port_min,
-				  unsigned short port_max,
-				  sipe_listen_start_cb listen_cb,
-				  sipe_client_connected_cb connect_cb,
-				  gpointer data)
+sipe_backend_network_listen_range(unsigned short /*port_min*/,
+				  unsigned short /*port_max*/,
+				  sipe_listen_start_cb /*listen_cb*/,
+				  sipe_client_connected_cb /*connect_cb*/,
+				  gpointer /*data*/)
 {
     SIPE_DEBUG_INFO_NOFORMAT("sipe_backend_network_listen_range");
     return NULL;
 }
 
-void sipe_backend_network_listen_cancel(struct sipe_backend_listendata *ldata)
+void sipe_backend_network_listen_cancel(struct sipe_backend_listendata * /*ldata*/)
 {
     SIPE_DEBUG_INFO_NOFORMAT("sipe_backend_network_listen_cancel");
 }
 
-gboolean sipe_backend_fd_is_valid(struct sipe_backend_fd *fd)
+gboolean sipe_backend_fd_is_valid(struct sipe_backend_fd * /*fd*/)
 {
     SIPE_DEBUG_INFO_NOFORMAT("sipe_backend_fd_is_valid");
     return FALSE;
 }
 
-void sipe_backend_fd_free(struct sipe_backend_fd *fd)
+void sipe_backend_fd_free(struct sipe_backend_fd * /*fd*/)
 {
     SIPE_DEBUG_INFO_NOFORMAT("sipe_backend_fd_free");
 }
